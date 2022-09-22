9da5b62f5f817751622fa099b5da6ed4b0cf4a0b
Cláudio Oliveira <claudiooliveira@aridesa.com.br>
2022-09-22 13:37:22 -0300

    Inserindo Entidade Phone e adicionando relacionamento OneToMany

---
+++ src/Entity/Phone.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToOne;
+
+#[Entity]
+class Phone
+{
+    #[Id, Column, GeneratedValue]
+    public  int $id;
+
+    //o inverseBy é opcional para o relacionamento mas ajuda o Doctrine em alguns cenários.
+    #[ManyToOne(targetEntity: Student::class, inversedBy: "phones")]
+    public readonly Student $student;
+
+    public function __construct(
+
+        #[Column]
+        public  string $numberPhone,
+
+    ) {
+    }
+
+    public function setStudent(Student $student): void
+    {
+        $this->student = $student;
+    }
+}
--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -2,18 +2,23 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\OneToMany;
 
 #[Entity]
 class Student
 {
-    #[Id]
-    #[Column]
-    #[GeneratedValue]
+    #[Id, Column, GeneratedValue]
     public  int $id;
+
+
+    #[OneToMany(targetEntity: Phone::class, mappedBy: "student")]
+    public  readonly Collection $phones;
 
     public function __construct(
 
@@ -21,5 +26,17 @@ class Student
         public  string $name,
 
     ) {
+        $this->phones = new ArrayCollection();
+    }
+
+    public function addPhone(Phone $phone)
+    {
+        $this->phones->add($phone);
+        $phone->setStudent($this);
+    }
+
+    public function phones(): iterable
+    {
+        return $this->phones;
     }
 }
