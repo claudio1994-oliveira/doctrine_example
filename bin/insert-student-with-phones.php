68d35665d4556daff7feaf7f68f51fcbe85a0b57
Cláudio Oliveira <claudiooliveira@aridesa.com.br>
2022-09-23 13:20:37 -0300

    Refatorando inserção e exclusão de aluno para cascade

--- bin/insert-student-with-phones.php
+++ bin/insert-student-with-phones.php
@@ -14,13 +14,10 @@ $phone1 = new Phone('(85) 99966-8899');
 $phone2 = new Phone('(85) 99566-8892');
 $phone3 = new Phone('(85) 98566-8469');
 
-$entityManager->persist($phone1);
-$entityManager->persist($phone2);
-$entityManager->persist($phone3);
 
-$student->addPhone($phone1);
-$student->addPhone($phone2);
-$student->addPhone($phone3);
+$student->addPhone(new Phone('(85) 99966-8899'));
+$student->addPhone(new Phone('(85) 99566-8892'));
+$student->addPhone(new Phone('(85) 99566-8892'));
 
 $entityManager->persist($student);
 $entityManager->flush();
--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -17,7 +17,7 @@ class Student
     public  int $id;
 
 
-    #[OneToMany(targetEntity: Phone::class, mappedBy: "student")]
+    #[OneToMany(targetEntity: Phone::class, mappedBy: "student", cascade: ['persist', 'remove'])]
     public  Collection $phones;
 
     public function __construct(
